a2cbe00977c6264f745cdf436e9c7af4e40dc095
sanganhsatwe123 <levasang51@gmail.com>
2023-04-20 11:24:01 +0700

    add more audio to the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -9,10 +10,29 @@ public class GameManager : MonoBehaviour
     public Pacman pacman;
     public Transform pellets;
     public AudioSource intro;
+    public AudioSource siren;
+    public AudioSource munch1;
+    public AudioSource munch2;
+    public AudioSource munchGhost;
+    public AudioSource cleared;
+    public AudioSource Gameover;
+    public AudioSource pacmandeath;
+    public int currentMunch = 0;
+
+    public TMP_Text highScoreT;
+    public TMP_Text Score;
+    public TMP_Text gameOver;
+    public TMP_Text livesTxt;
 
     public int score { get; private set;}
+    public int hiScore { get; private set; }
     public int lives { get; private set;}
     public int ghostMultiplier { get; private set;}
+
+    private void Awake()
+    {
+        currentMunch = 0;
+    }
 
     private void Start()
     {
@@ -25,10 +45,13 @@ public class GameManager : MonoBehaviour
         SetScore(0);
         SetLives(3);
         NewRound();
+        siren.Play();
+        livesTxt.text = ("3");
     }
 
     private void NewRound()
     {
+        gameOver.enabled = false;
         foreach (Transform pellet in this.pellets)
         {
             pellet.gameObject.SetActive(true);
@@ -43,6 +66,12 @@ public class GameManager : MonoBehaviour
         {
             NewGame();
         }
+        if(score > hiScore)
+        {
+            hiScore = score;
+            highScoreT.text = hiScore.ToString();
+        }
+        
     }
 
     private void ResetState()
@@ -57,16 +86,21 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
+        gameOver.enabled = true;
         for (int i = 0; i < this.ghosts.Length; i++)
         {
             this.ghosts[i].gameObject.SetActive(false);
         }
         this.pacman.gameObject.SetActive(false);
+        Gameover.Play();
+        siren.Pause();
+        livesTxt.text = "0";
     }
 
     private void SetScore(int score)
     {
         this.score = score;
+        Score.text = score.ToString();
     }
 
     private void SetLives(int lives)
@@ -79,6 +113,7 @@ public class GameManager : MonoBehaviour
         int points = ghost.points * this.ghostMultiplier;
         SetScore((int)this.score + points);
         this.ghostMultiplier++;
+        munchGhost.Play();
     }
 
     public void PacmanEaten()
@@ -88,6 +123,8 @@ public class GameManager : MonoBehaviour
         if(this.lives > 0)
         {
             Invoke(nameof(ResetState), 3.0f);
+            livesTxt.text = this.lives.ToString();
+            pacmandeath.Play();
         }
         else
         {
@@ -99,10 +136,27 @@ public class GameManager : MonoBehaviour
     {
         SetScore(this.score + pellet.points);
         pellet.gameObject.SetActive(false);
+
+        if(currentMunch == 0)
+        {
+            munch1.Play();
+            currentMunch = 1;
+        }
+        else
+        {
+            if(currentMunch == 1)
+            {
+                munch2.Play();
+                currentMunch = 0;
+            }
+        }
+
         if (!HasRemainningPellets())
         {
-            Invoke(nameof(NewRound), 3.0f);
-            this.pacman.gameObject.SetActive(false) ;
+            Invoke(nameof(NewRound), 6.0f);
+            cleared.Play();
+            siren.Pause();
+            this.pacman.gameObject.SetActive(false);
         }
     }
 
